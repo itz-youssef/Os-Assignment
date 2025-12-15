e869d75535bf3540f5b2905ce488a6131d42cceb
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2025-12-15 20:39:10 +0200

    added the RR scheduler (working but not complete yet)

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     private class Process {
         private String id;
         private int burstTime;
         private int arrivalTime;
         private int priority;
-        public Process(String id, int burstTime, int arrivalTime, int quantum) {
+        public Process(String id, int burstTime, int arrivalTime, int priority) {
             this.id = id;
             this.burstTime = burstTime;
             this.arrivalTime = arrivalTime;
-            this.priority = quantum;
+            this.priority = priority;
         }
         public String getId() {return id;}
         public int getBurstTime() {return burstTime;}
@@ -17,5 +21,55 @@ public class Main {
         public void setBurstTime(int burstTime) {this.burstTime = burstTime;}
         public void setArrivalTime(int arrivalTime) {this.arrivalTime = arrivalTime;}
         public void setPriority(int priority) {this.priority = priority;}
+    }
+
+    private class RRScheduler {
+        private Queue<Process> processQueue;
+        private int currentTime;
+        private final int quantum;
+        public RRScheduler(ArrayList<Process> processes, int quantum) {
+            this.processQueue = new LinkedList<>();
+            // sorting on process arrival time
+            int numProcesses = processes.size();
+            boolean swapped;
+            for (int i = 0; i < numProcesses-1; i++) {
+                swapped = false;
+                for (int j = 0; j < numProcesses - 1 - i; j++) {
+                    if (processes.get(j).getArrivalTime() > processes.get(j+1).getArrivalTime()) {
+                        Process temp = processes.get(j);
+                        processes.set(j, processes.get(j+1));
+                        processes.set(j+1, temp);
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+            processQueue.addAll(processes);
+            this.currentTime = 0;
+            this.quantum = quantum;
+        }
+        public void simulate() {
+            Process nextProcess;
+            Process currentProcess = null;
+            while (!processQueue.isEmpty()) {
+                nextProcess = processQueue.peek();
+                if (nextProcess.getArrivalTime() <= currentTime) {
+                    currentProcess = processQueue.poll();
+                }
+                else if (currentProcess != null) {
+                    currentTime = nextProcess.getArrivalTime();
+                    continue;
+                }
+                int currentBurstTime = currentProcess.getBurstTime();
+                for (int i = 0; i < Math.min(quantum, currentBurstTime); i++) {
+                    System.out.println("Time: " + currentTime + ", processing " + currentProcess.getId());
+                    currentTime++;
+                    currentProcess.setBurstTime(currentProcess.getBurstTime()-1);
+                }
+                if (!processQueue.isEmpty()) System.out.println("Time: " + currentTime + ", Switching to Process " + processQueue.peek().getId());;
+                if (currentProcess.getBurstTime() > 0) processQueue.add(currentProcess);
+            }
+            System.out.println("Time: " + currentTime + ", Finished All Processes!!!");
+        }
     }
 }
