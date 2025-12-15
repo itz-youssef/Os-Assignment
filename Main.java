04f918e0a9f4c9e1ccf2597717e61061cb26d22a
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2025-12-15 06:05:57 +0200

    small fix in the process class

--- Main.java
+++ Main.java
@@ -3,19 +3,19 @@ public class Main {
         private String id;
         private int burstTime;
         private int arrivalTime;
-        private int quantum;
+        private int priority;
         public Process(String id, int burstTime, int arrivalTime, int quantum) {
             this.id = id;
             this.burstTime = burstTime;
             this.arrivalTime = arrivalTime;
-            this.quantum = quantum;
+            this.priority = quantum;
         }
         public String getId() {return id;}
         public int getBurstTime() {return burstTime;}
         public int getArrivalTime() {return arrivalTime;}
-        public int getQuantum() {return quantum;}
+        public int getPriority() {return priority;}
         public void setBurstTime(int burstTime) {this.burstTime = burstTime;}
         public void setArrivalTime(int arrivalTime) {this.arrivalTime = arrivalTime;}
-        public void setQuantum(int quantum) {this.quantum = quantum;}
+        public void setPriority(int priority) {this.priority = priority;}
     }
 }
